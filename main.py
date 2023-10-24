7516e03065e1378910e0af873d07fe01dc9d86a3
Dragan <milosevicdragan002@gmail.com>
2023-10-24 19:08:24 +0200

    Create model

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ mnist = tf.keras.datasets.mnist
 X_train,X_test = X_train/255.0,X_test/255.0
 print("X_train.shape:",X_train.shape)
 
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape = (28,28)),
+    tf.keras.layers.Dense(128,activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10,activation = 'softmax')
+])
+
+
