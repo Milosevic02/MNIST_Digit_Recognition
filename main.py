c25d783cdf350a9f40deda90d945dcfe23974793
Dragan <milosevicdragan002@gmail.com>
2023-10-24 19:11:35 +0200

    plot accuracy and confusion matrix

--- main.py
+++ main.py
@@ -16,3 +16,51 @@ model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['
 
 r = model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=10)
 
+import matplotlib.pyplot as plt
+
+plt.plot(r.history['loss'],label = 'loss')
+plt.plot(r.history['val_loss'],label = 'val_loss')
+plt.show()
+
+plt.plot(r.history['accuracy'],label = 'accuracy')
+plt.plot(r.history['val_accuracy'],label = 'accuracy')
+plt.show()
+
+
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import itertools
+
+def plot_confusion_matrix(cm,classes,normalize=False,title = "Confusion Matrix",cmap = plt.cm.Blues):
+
+  if normalize:
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    print("Normalized confusion matrix")
+  else:
+    print('confusion matrix,without normalization')
+
+  print(cm)
+
+  plt.imshow(cm,interpolation='nearest',cmap = cmap)
+  plt.title(title)
+  plt.colorbar()
+  tick_marks = np.arange(len(classes))
+  plt.xticks(tick_marks,classes,rotation = 45)
+  plt.yticks(tick_marks,classes)
+
+  fmt = '.2f' if normalize else 'd'
+  thresh = cm.max() / 2.
+
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(j, i, format(cm[i, j], fmt),
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+  plt.tight_layout()
+  plt.ylabel('True label')
+  plt.xlabel('Predicted label')
+  plt.show()
+
+p_test = model.predict(X_test).argmax(axis = 1)
+cm = confusion_matrix(y_test,p_test)
+plot_confusion_matrix(cm,list(range(10)))
